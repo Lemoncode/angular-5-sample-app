89fa09fe5f4136ad05d8f71efabd2fdee2b33c2e
JaimeSalas <jaime.salas@lemoncode.net>
2018-01-17 16:24:59 +0100

    Modify package.json

--- Demos/00 HelloWorld/game-catalog/src/app/app.component.ts
+++ Demos/00 HelloWorld/game-catalog/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title: string = null;
+
+  ngOnInit(): void {
+    this.title = 'app works';
+  }
 }
